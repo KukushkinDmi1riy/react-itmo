70ce4814f967367c0addd2fda18dbe7d74f57676
KukushkinDmi1riy <42671583+KukushkinDmi1riy@users.noreply.github.com>
2023-09-29 13:15:31 +0600

    Add files via upload

---
+++ precache-manifest.f13e0e6fd561bd53516ea46e2f3db840.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d80de9f0da5a5fbd5fb74f7c302154a9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9a77f60b9d06abeeb472",
+    "url": "/static/css/main.a9089c74.chunk.css"
+  },
+  {
+    "revision": "f8cda03a0698feb3be5b",
+    "url": "/static/js/2.612d2c84.chunk.js"
+  },
+  {
+    "revision": "9a77f60b9d06abeeb472",
+    "url": "/static/js/main.719bb042.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
